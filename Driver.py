ecde749ec8d8aa93d4c20959f41b5b87c0c6e169
NoaShtober <94970175+NoaShtober@users.noreply.github.com>
2023-05-02 16:16:31 +0300

    Create Driver.py

---
+++ Driver.py
@@ -0,0 +1,7 @@
+from University.Person import person
+
+
+class driver(person):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
